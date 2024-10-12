cb7c8e420ecf4dcfa962585e6fc03d8c78dea2d4
vladysl4v <arkhypenkov.corp@gmail.com>
2024-10-12 16:34:01 +0300

    client: create page header and add favicon

---
+++ client/src/layouts/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div className="w-full">
+            <header className="border-t-4 border-yellow-400 py-4">
+                <div className='border-b-[1px] border-gray-300'>
+                    <div className="container mx-auto flex justify-between items-center mb-4">
+                        <div className='flex flex-row w-48'>
+                            <img src='/favicon.svg' className='max-h-[52px]' alt="Logo" />
+                            <div className='my-auto mx-auto'>
+                                <span className="my-auto text-xl font-bold">YANA</span>
+                                <div className='text-xs'>Yet Another Notes App</div>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="bg-transparent py-1 px-3 rounded-lg mr-2 hover:bg-gray-200 transition-all duration-200">Login</button>
+                            <button className="bg-transparent py-2 px-4 border-2 border-yellow-500 hover:bg-yellow-500 transition-all duration-200 hover:text-white hover:font-thin rounded-xl font-bold mr-2">Register</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 7570
+    port: 3070
   }
 })
